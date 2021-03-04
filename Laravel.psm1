f40922918ddb37b1e5ae4ea2d4149aa97032d4b2
Dombi Dávid <dombidav@gmail.com>
2021-03-04 12:02:57 +0100

    Init

---
+++ Laravel.psm1
@@ -0,0 +1,160 @@
+function test  {
+    param(
+        # Function
+        [Parameter(Mandatory=$false)]
+        $innerFunc,
+
+        # Function
+        [Parameter(Mandatory=$false)]
+        $param
+    )
+    if(!$innerFunc){
+        vendor/bin/phpunit --coverage-html tests/Coverage
+        Start-Process './tests/Coverage/index.html'
+    }
+    elseif($innerFunc -eq 'ignore'){
+        $file = 'phpunit.xml'
+        $regex = '( *)<!-- Excluded directories -->'
+        $tag = 'file'
+        if((Get-Item $param) -is [System.IO.DirectoryInfo]){
+            $tag = 'directory'
+        }
+        $param = $param.TrimEnd('\').TrimEnd('/') -replace '\\', '/'
+        $result = (Get-Content $file) -replace $regex, "`$1<!-- Excluded directories -->`n`$1<$tag>$param</$tag>" # | Write-Host #Set-Content $file
+        Set-Content $file $result
+        Write-Output "Coverage test will ignore $tag '$param'"
+    }
+    elseif($innerFunc -eq 'include'){
+        $file = 'phpunit.xml'
+        $regex = '( *)<!-- Included directories -->'
+        $tag = 'file'
+        if((Get-Item $param) -is [System.IO.DirectoryInfo]){
+            $tag = 'directory'
+        }
+        $param = $param.TrimEnd('\').TrimEnd('/') -replace '\\', '/'
+        $result = (Get-Content $file) -replace $regex, "`$1<!-- Included directories -->`n`r`$1<$tag>$param</$tag>" # | Write-Host #Set-Content $file
+        Set-Content $file $result
+        Write-Output "Coverage test will include $tag '$param'"
+    }
+    else {
+        Write-Error 'Unknown command'
+    }
+}
+
+function Get-ServerName {
+    $hash = Import-Env ".env"
+    return $hash.APP_URL -replace 'https?://', ''
+}
+
+function Add-Vhosts {
+    $dir = $pwd -replace '\\', '/'
+    $content = "`n`n" + 
+    '<VirtualHost *:80>
+        ServerName '+ (Get-ServerName) + '
+        DocumentRoot "' + $dir + '/public"
+        SetEnv APPLICATION_ENV "development"
+        <Directory "' + $dir + '">
+            DirectoryIndex index.php
+            AllowOverride All
+            Order allow,deny
+            Allow from all
+        </Directory>
+    </VirtualHost>'
+    Add-Content -Path $Global:LumenAPI_ENV.VHOSTS -Value $content
+}
+
+function Add-Hosts {
+    $content = "`n" + 
+    '127.0.0.1 '+ (Get-ServerName)
+    Add-Content -Path $Global:LumenAPI_ENV.HOSTS -Value $content
+}
+
+function lumen {
+    param (
+        # Function
+        [Parameter(Mandatory=$True, Position = 0)]
+        [string]
+        $func,
+
+        [Parameter(
+            Mandatory=$false,
+            ValueFromRemainingArguments=$true,
+            Position = 1
+        )][string[]]
+        $params
+    )
+    $ErrorActionPreference = "Inquire"
+    $Global:LumenAPI_ENV = Import-Env "$PSScriptRoot\lumen.env"
+    if(!$func){
+        php artisan
+    }
+    elseif ($func -eq 'admin') {
+        Start-Process -FilePath $Global:LumenAPI_ENV.SQL_ADMIN
+    }
+	elseif ($func -eq 'conf'){
+		code "$PSScriptRoot\lumen.env"
+	}
+    elseif (($func -eq 'compose')) {
+        composer install
+        yarn
+        php artisan migrate:fresh --seed
+        yarn run development
+    }
+    elseif (($func -eq 'db:refresh') -or ($func -eq 'db')) {
+        php artisan migrate:fresh --seed
+    }
+	elseif ($func -eq 'db:fresh') {
+        php artisan migrate:fresh
+    }
+    elseif(($func -eq 'compile') -or ($func -eq 'c')){
+        yarn run development
+    }
+    elseif($func -eq 'coverage'){
+        Start-Process ".\tests\Coverage\index.html"
+    }
+    elseif($func -eq 'test'){
+        if($params.Length -eq 1){
+            test($params[0])
+        }
+        elseif($params.Length -eq 2){
+            test $params[0] $params[1]
+        }
+        else {
+            test
+        }
+    }
+    elseif ($func -eq 'vhosts'){
+        if($params.Length -gt 0){
+            if(($params[0] -eq 'reg') -or $params[0] -eq 'register'){
+                Add-Vhosts
+            }
+        }
+        else{
+            code $Global:LumenAPI_ENV.VHOSTS
+        }
+    }elseif ($func -eq 'hosts') {
+        if($params.Length -gt 0){
+            if(($params[0] -eq 'reg') -or $params[0] -eq 'register'){
+                Add-Hosts
+            }
+        }
+        else{
+            code $Global:LumenAPI_ENV.HOSTS
+        }  
+    }
+    elseif ($func -eq 'sql') {
+        $hash = Import-Env ".env"
+        $arguments = ("-u "+ $hash.DB_USERNAME, "--database " + $hash.DB_DATABASE)
+        if($hash.DB_PASSWORD){
+            $arguments = ("-u "+ $hash.DB_USERNAME, "-p " + $hash.DB_PASSWORD ,"--database " + $hash.DB_DATABASE)
+        }
+        Start-Process $Global:LumenAPI_ENV.SQL -ArgumentList ("-u "+ $hash.DB_USERNAME, "--database " + $hash.DB_DATABASE) -NoNewWindow -Wait
+    }
+    else {
+        $a = $PsBoundParameters.Values + $args
+        php artisan $a
+    }
+    
+}
+
+Export-ModuleMember -Function New-Laravel, New-Lumen, lumen
---
+++ README.md
@@ -0,0 +1,145 @@
+# About PS-Mod: Laravel
+This is a helper function collection for Laravel and Lumen frameworks
+
+# Configuration
+This package uses the `lumen.env` file for configuration. It resides in the package folder. (`C:\tools\ps-mod\Laravel` by default) You can access it with the command `lumen conf`.
+> Note: When configuring SQL_ADMIN you can also specify executable location
+
+# Commands
+## admin
+Opens the default database admin GUI
+
+### Usage
+```ps
+lumen admin
+```
+
+## conf
+Opens the configuration file for the script collection.
+
+### Usage
+```ps
+lumen conf
+```
+
+## compose
+Runs the following commands:
+- composer install
+- yarn
+- php artisan migrate:fresh --seed
+- yarn run development
+
+Usefull when you start developing for the day.
+
+### Usage
+```ps
+lumen compose
+```
+
+## db:fresh
+Drops all migration and reruns all. Equivalent to `php artisan migrate:fresh`
+
+### Usage
+```ps
+lumen db:fresh
+```
+
+## db:refresh / db
+Drops all migration, reruns all, then runs DatabaseSeeder. Equivalent to `php artisan migrate:fresh --seed`
+
+### Usage
+```ps
+lumen db:refresh
+# or:
+lumen db
+```
+
+## compile / c
+Recompiles scss, sass and js files. Equivalent to `yarn run development`
+
+### Usage
+```ps
+lumen compile
+# or:
+lumen c
+```
+
+## coverage
+Opens test coverage report
+
+### Usage
+```ps
+lumen coverage
+```
+
+## vhosts
+Opens apache `httpd-vhosts.conf` file.
+
+### Usage
+```ps
+lumen vhosts
+```
+
+## vhosts register / vhosts reg
+Registers the current app as a vhost
+
+### Usage
+```ps
+lumen vhosts register
+# or:
+lumen vhosts reg
+```
+
+## hosts
+Opens the `C:\Windows\System32\drivers\etc\hosts` file.
+
+### Usage
+```ps
+lumen hosts
+```
+
+## hosts register / hosts reg
+Registers the current app as a DNS address
+
+### Usage
+```ps
+lumen hosts register
+# or:
+lumen hosts reg
+```
+
+## test
+Runs PHP Unit and Feature tests and creates a coverage report in html format.
+
+### Usage
+```ps
+lumen test
+```
+
+## test include
+Includes a specific file or directory in test coverage report
+
+### Syntax
+```
+lumen test include <Path>
+```
+
+### Usage
+```ps
+lumen test include MyDirectory
+lumen test include MyFile
+```
+
+## test ignore
+Excludes a specific file or directory from test coverage report
+
+### Syntax
+```
+lumen test ignore <Path>
+```
+
+### Usage
+```ps
+lumen test ignore MyDirectory
+lumen test ignore MyFile
+```
